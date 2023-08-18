0a329ed9148620f39639e6211e12da70ea3cd1a3
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-18 14:23:20 +0630

    add email sent

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,27 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_pqaxh4i', 'template_mb1totj', form.current, 'eQPIKWGNgXy0ZNakn')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <div id="contact" className="w-full min-h-screen">
       <h3 className="text-3xl py-1 dark:text-white ">Contact</h3>
-      <form className="flex flex-col justify-center items-center mt-5 ">
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col justify-center items-center mt-5 ">
         <div className="relative z-0 mb-6 group w-[400px]">
           <input
             type="text"
-            name="floating_name"
+            name="from_name"
             id="floating_name"
             className="w-full block py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
@@ -22,7 +37,7 @@ const Contact = () => {
         <div className="relative z-0 mb-6 group w-[400px]">
           <input
             type="email"
-            name="floating_email"
+            name="from_email"
             id="floating_email"
             className="w-full block py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
@@ -38,7 +53,7 @@ const Contact = () => {
         <div className="relative z-0 mb-6 group w-[400px]">
           <textarea
             type="text"
-            name="floating_messages"
+            name="messages"
             id="floating_messages"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
