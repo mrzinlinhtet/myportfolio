1a05ecc0adf2bfd1040d4748652817f8b025619b
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-21 13:36:55 +0630

    add btn live

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,7 @@ import { BsFileArrowUpFill } from "react-icons/bs";
 import ScrollToTop from "react-scroll-up";
 import { BsGithub } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { RiLiveLine } from "react-icons/ri";
 
 const Projects = () => {
   const darkMode = useSelector((state) => state.darkMode.isDarkMode);
@@ -113,28 +114,52 @@ const Projects = () => {
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     Duration: 2 days
                   </p>
-                  <Link
-                    to="https://github.com/zinlinxtet/ecommerce-proj"
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-700 rounded-lg hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-                  >
-                    <BsGithub className="text-xl me-3" />
-                    CODE
-                    <svg
-                      className="w-3.5 h-3.5 ml-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 10"
+                  <div className="flex justify-between">
+                    <Link
+                      to="https://github.com/zinlinxtet/ecommerce-proj"
+                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-700 rounded-lg hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
                     >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
-                    </svg>
-                  </Link>
+                      <BsGithub className="text-xl me-3" />
+                      CODE
+                      <svg
+                        className="w-3.5 h-3.5 ml-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                      </svg>
+                    </Link>
+                    <Link
+                      to="https://gamani-store.vercel.app"
+                      className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-700 rounded-lg hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                    >
+                      <RiLiveLine className="text-xl me-3" />
+                      LIVE
+                      <svg
+                        className="w-3.5 h-3.5 ml-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                      </svg>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
