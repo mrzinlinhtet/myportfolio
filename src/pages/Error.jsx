1379db2b47d14a3e8128b28bb117bbaafe0f607d
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-23 11:23:52 +0630

    fix darkmode in error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -6,7 +6,7 @@ const Error = () => {
   return (
     <div className="">
       <section id="projects" className={darkMode ? "dark" : ""}>
-        <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-10 mt-18 pt-20">
+        <main className="min-h-screen bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-10 mt-18 pt-20">
           <div className="flex justify-center items-center">
             <img src="404.svg" alt="" className="w-1/2"/>
           </div>
