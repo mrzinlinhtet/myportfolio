c450409766be49adb32a4902af68938168767fcd
zinlinxtet <devzinlinhtet@gmail.com>
2023-08-28 14:40:12 +0630

    add react type animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -78,7 +78,7 @@ const Contact = ({ notify }) => {
             htmlFor="floating_email"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-cyan-600 peer-focus:dark:text-cyan-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Email address
+            Email
           </label>
         </div>
         <div className="relative z-0 mb-6 group w-full md:w-[400px]">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -43,7 +43,7 @@ const Skills = () => {
     },
     {
       name: "MySQL",
-      progress: "66%",
+      progress: "70%",
     },
   ];
 
