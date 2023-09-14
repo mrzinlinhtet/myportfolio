195b99e0da40f7e0801a53d144e5eb7f2e14c01f
zinlinxtet <devzinlinhtet@gmail.com>
2023-09-14 09:46:03 +0630

    change github url

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Social = () => {
   return (
     <div className="relative md:fixed top-[320px] md:left-6 md:top-[350px] text-3xl flex items-center justify-center md:flex-col md:justify-start gap-10 py-3 text-gray-600 dark:text-gray-400">
-      <Link to="https://github.com/zinlinxtet">
+      <Link to="https://github.com/mrzinlinhtet">
         <AiFillGithub />
       </Link>
       <Link to="https://www.linkedin.com/in/zinlinhtet/" >
