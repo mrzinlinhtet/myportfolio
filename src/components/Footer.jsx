4d3b2a2afd58a38aab03c3d62c552a2afd713fb3
“mrzinlinhtet” <devzinlinhtet@gmail.com>
2024-03-06 13:34:54 +0630

    update 2024

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="flex justify-center md:block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023 ZLH . All Rights Reserved.
+            © 2024 ZLH . All Rights Reserved.
           </span>
         </div>
       </footer>
