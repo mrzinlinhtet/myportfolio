5d0fc9d4d9e14a5e656eb3836352d84968ee87d1
“mrzinlinhtet” <devzinlinhtet@gmail.com>
2025-11-17 21:53:02 +0900

    update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,22 +19,14 @@ const About = () => {
           <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
             I was graduated with BE in Information Technology from Technological University
             (Mandalay). Transitioning from Telecom field, where I have 3 years of experience, I have been working as
-            a Web Developer for about 2 year at Bliss Stock and Brycen Myanmar. Currently working at
-            <span className="text-cyan-500"> Fixie </span> as a Web
+            a Web Developer for about 3 years using Java. Currently working at
+            <span className="text-cyan-500"> Human Resocia </span> as a Web
             Developer specialized in Java development.
           </p>
           <p className="text-md py-2 mb-6 leading-8 text-gray-800 dark:text-gray-200">
             In the dynamic IT landscape, I&apos;m dedicated to continuous learning
             and innovation.
           </p>
-          <a
-            href="/ZinLinHtetCV.pdf"
-            download
-            className="mb-5 md:mb-0 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cyan-700 rounded-lg hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-          >
-            Download CV
-            <BiDownload className="text-xl ms-2" />
-          </a>
         </div>
       </section>
     </>
